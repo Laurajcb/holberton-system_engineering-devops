386e28bdc38cb280410c3222992afe2ea566e324
laurajcb <callejaslaura96@gamil.com>
2022-01-04 00:15:03 +0000

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,5 @@ file { '/tmp/school':
     ensure  => 'present',
     mode    => '0744',
     group   => 'www-data',
-    owner   => ' www-data',
-    content => 'I love Puppet'
-}
+    owner   => 'www-data',
+    content => 'I love Puppet',}
