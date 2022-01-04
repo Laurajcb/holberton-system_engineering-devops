5ba6b7e37117baee0e9a25a0ceb91dbe83bcc2c5
laurajcb <callejaslaura96@gamil.com>
2022-01-04 00:11:56 +0000

    create point 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet create a file in /tmp.
+
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    group   => 'www-data',
+    owner   => ' www-data',
+    content => 'I love Puppet'
+}
