bb2da9c972081bc5cddfbe72fec7b9eb6aefe38f
laurajcb <callejaslaura96@gmail.com>
2022-02-15 17:17:08 +0000

    ADDED top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module to that queries and returns
+    thefist 10 hot post of a given subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and returns the 10 first 
+    for a given subreddit."""
+
+    reddit_url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent'}
+    response_request = requests.get(
+        reddit_url, headers=headers, allow_redirects=False)
+
+    if response_request.status_code == 200:
+        data = response_request.json()['data']['children']
+        for posts in data[:10]:
+            print(posts['data']['title'])
+
+    else:
+        print('None')
